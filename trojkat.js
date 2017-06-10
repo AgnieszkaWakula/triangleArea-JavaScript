b1a00ca2887a90d786ce3d713a58ccf2a953c6b5
Agnieszka Wakula <agnieszka.wakula@gmail.com>
2017-06-10 13:51:14 +0200

    kodilla 8.5

---
+++ trojkat.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = 5;
+var h = 2;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var wynik = prompt(triangleArea)
